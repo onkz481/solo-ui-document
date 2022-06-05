4b83686ddc9ead788e997abbc4921bc4bd2370fe
小野 和也 <prvogo@gmail.com>
2022-06-05 11:02:42 +0900

    モバイルモード時にホームのボタン配置がずれる問題の修正

--- src/components/home/actionButtons.vue
+++ src/components/home/actionButtons.vue
@@ -3,28 +3,36 @@
     <su-row
       justify="center"
     >
-      <su-btn
-        class="mr-4 text-decoration--none"
-        color="primary"
-        width="250"
-        large
-        :to="`/introductions/Install`"
+      <su-col
+        cols="auto"
       >
-        <su-icon>mdi-flag</su-icon>
-        {{ $soloui.lang.t('doc-titles.introductions.documents.Install') }}
-      </su-btn>
+        <su-btn
+          class="text-decoration--none"
+          color="primary"
+          width="250"
+          large
+          :to="`/introductions/Install`"
+        >
+          <su-icon>mdi-flag</su-icon>
+          {{ $soloui.lang.t('doc-titles.introductions.documents.Install') }}
+        </su-btn>
+      </su-col>
 
-      <su-btn
-        class="text-decoration--none"
-        color="secondary"
-        width="250"
-        large
-        to="https://github.com/onkz481/solo-ui"
-        target="_blank"
+      <su-col
+        cols="auto"
       >
-        <su-icon>mdi-github</su-icon>
-        GitHub
-      </su-btn>
+        <su-btn
+          class="text-decoration--none"
+          color="secondary"
+          width="250"
+          large
+          to="https://github.com/onkz481/solo-ui"
+          target="_blank"
+        >
+          <su-icon>mdi-github</su-icon>
+          GitHub
+        </su-btn>
+      </su-col>
     </su-row>
   </section>
 </template>
