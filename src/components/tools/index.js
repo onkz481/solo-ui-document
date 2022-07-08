c232eb5a1feac72fe792f346d7a58fba37829250
小野 和也 <prvogo@gmail.com>
2022-07-08 21:46:06 +0900

    「table」タグをsu-tableに変換するコンポーネントの追加

---
+++ src/components/tools/index.js
@@ -0,0 +1,15 @@
+import docInfo from './docInfo.vue'
+import optionsTable from './optionsTable.vue'
+import rLink from './rLink'
+
+export {
+  docInfo,
+  optionsTable,
+  rLink
+}
+
+export default {
+  docInfo,
+  optionsTable,
+  rLink
+}
---
+++ src/components/tools/optionsTable.vue
@@ -0,0 +1,40 @@
+<template>
+  <su-card
+    elevation="0"
+    outlined
+  >
+    <su-table
+      :headers="headers"
+      :items="items"
+      :items-per-page="-1"
+      hide-footer
+    />
+  </su-card>
+</template>
+
+<script>
+import { convertTableToObject } from '../../util/markdown'
+
+export default {
+  name: 'OptionsTable',
+  props: {
+    html: {
+      type: String,
+      default: undefined
+    }
+  },
+  data: () => ({
+    headers: [],
+    items: []
+  }),
+  mounted(){
+    const tableState = convertTableToObject(this.html)
+
+    this.headers = tableState.headers
+    this.items = tableState.items
+  },
+  methods: {
+    convertTableToObject
+  }
+}
+</script>
